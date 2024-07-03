a7170643c02e7438f7d5fe9ae7f3ec6b9722a975
gac <info@algos.it>
2024-07-03 10:23:49 +0200

    image

--- src/main/java/it/algos/geo/boot/GeoMenu.java
+++ src/main/java/it/algos/geo/boot/GeoMenu.java
@@ -1,6 +1,5 @@
 package it.algos.geo.boot;
 
-import it.algos.vbase.backend.annotation.AMenu;
 import it.algos.vbase.backend.constant.Gruppo;
 import it.algos.vbase.backend.service.LayoutService;
 import it.algos.vbase.backend.tree.TreeNode;
@@ -17,9 +16,10 @@ public class GeoMenu {
     LayoutService layoutService;
 
 
-    @AMenu
+//    @AMenu
     public TreeNode buildMenu(TreeNode treeNode) {
         layoutService.fixOrderGroup(treeNode, Arrays.asList("Anagrafica", "Crono", "Geo", "Utility", "Task", "Test"));
+//        layoutService.fixOrderGroup(treeNode, Arrays.asList( "Geo", "Utility"));
         layoutService.fixNomeGruppo(treeNode, Gruppo.GEO, "Geografia");
         return treeNode;
     }
