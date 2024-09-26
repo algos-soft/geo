034e42e6adc32a718c151570a01068e82054b938
gac <info@algos.it>
2024-09-26 20:25:52 +0200

    ...

--- src/main/java/it/algos/geo/comune/ComuneForm.java
+++ src/main/java/it/algos/geo/comune/ComuneForm.java
@@ -1,6 +1,7 @@
 package it.algos.geo.comune;
 
 import com.vaadin.flow.spring.annotation.SpringComponent;
+import it.algos.vbase.backend.entity.AbstractEntity;
 import it.algos.vbase.backend.form.DefaultForm;
 import org.springframework.context.annotation.Scope;
 
@@ -8,12 +9,12 @@ import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROT
 
 @SpringComponent
 @Scope(value = SCOPE_PROTOTYPE)
-public class ComuneForm extends DefaultForm<ComuneEntity> {
+public class ComuneForm<T extends AbstractEntity> extends DefaultForm<T> {
 
 
-    public ComuneForm(ComuneEntity bean) {
+    public ComuneForm(T bean) {
         super(bean);
     }
 
 
-}// end of CrudForm class
+}
